606a89e75d01aa7e36941840ac5b7cc60ddedad1
Day1721 <puzynamarek97@gmail.com>
2017-03-08 23:41:53 +0100

    Initial Commit

---
+++ CalculatorExample/CalculatorExample/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorExample.ViewModels;
+using Xamarin.Forms;
+
+namespace CalculatorExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public ResultViewModel Result { get; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Result = new ResultViewModel {NumericalResult = 5};
+            BindingContext = Result;
+            Zero.Clicked += NumButtonClickedFactory(0);
+            One.Clicked += NumButtonClickedFactory(1);
+            Two.Clicked += NumButtonClickedFactory(2);
+            Three.Clicked += NumButtonClickedFactory(3);
+            Four.Clicked += NumButtonClickedFactory(4);
+            Five.Clicked += NumButtonClickedFactory(5);
+            Six.Clicked += NumButtonClickedFactory(6);
+            Seven.Clicked += NumButtonClickedFactory(7);
+            Eight.Clicked += NumButtonClickedFactory(8);
+            Nine.Clicked += NumButtonClickedFactory(9);
+        }
+
+        private void ClearButtonClicked(object sender, EventArgs e)
+        {
+            Result.NumericalResult = 0;
+        }
+
+        private EventHandler NumButtonClickedFactory(int number)
+        {
+            return delegate
+            {
+                Result.NumericalResult *= 10;
+                Result.NumericalResult += number;
+            };
+        }
+
+        private void PlusMinusButtonClicked(object sender, EventArgs e)
+        {
+            Result.NumericalResult *= -1;
+        }
+
+        private void PercentButtonClicked(object sender, EventArgs e)
+        {
+            Result.NumericalResult /= 100;
+        }
+
+        private void DivisionButtonClicked(object sender, EventArgs e)
+        {
+            if (Result.NumericalTemp == null)
+            {
+                Result.NumericalTemp = Result.NumericalResult;
+                Result.NumericalResult = 0;
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
---
+++ CalculatorExample/CalculatorExample/ViewModels/ResultViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorExample.Annotations;
+
+namespace CalculatorExample.ViewModels
+{
+    public class ResultViewModel : INotifyPropertyChanged
+    {
+        public string Result => NumericalResult.ToString();
+
+        public double NumericalResult
+        {
+            get { return _numericalResult; }
+            set
+            {
+                _numericalResult = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+
+        public string TempResult 
+            => NumericalTemp == null ? string.Empty : NumericalTemp.Value.ToString();
+
+        public double? NumericalTemp
+        {
+            get { return _numericalTemp; }
+            set
+            {
+                _numericalTemp = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(TempResult));
+            }
+        }
+
+        private double? _numericalTemp;
+
+        private double _numericalResult;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static implicit operator double(ResultViewModel model)
+        {
+            return model.NumericalResult;
+        }
+
+        public static ResultViewModel operator *(ResultViewModel model, double value)
+        {
+            return new ResultViewModel {NumericalResult = value * model.NumericalResult};
+        }
+    }
+}
